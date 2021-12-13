4635741be60d6d6c9c7751851af63868043243d2
Lila Mullany <lilamullany@gmail.com>
2021-12-12 22:57:11 -0800

    initial commit

--- class_balancer.py
+++ class_balancer.py
@@ -1,22 +1,20 @@
-import argparse
 import os
 import shutil
-import re
 import xml.etree.ElementTree as et
-import json
-import re
 import edgeiq
 import random
 
 
 class ClassBalancer:
-    def __init__(self,input_dir):
-        self.path_annotations = os.path.join(input_dir,'Annotations')
-        self.path_images = os.path.join(input_dir,'JPEGImages')
-        self.path_holdout_dir = os.path.join(input_dir ,'holdout')
-        self.path_holdout_annotations = os.path.join(self.path_holdout_dir ,'Annotations')
-        self.path_holdout_images = os.path.join(self.path_holdout_dir,'JPEGImages')
-        self.dict_fnames_class ={}
+    def __init__(self, input_dir):
+        self.path_annotations = os.path.join(input_dir, 'Annotations')
+        self.path_images = os.path.join(input_dir, 'JPEGImages')
+        self.path_holdout_dir = os.path.join(input_dir, 'holdout')
+        self.path_holdout_annotations = os.path.join(
+            self.path_holdout_dir, 'Annotations')
+        self.path_holdout_images = os.path.join(
+            self.path_holdout_dir, 'JPEGImages')
+        self.dict_fnames_class = {}
         self.class_distribution = {}
 
     def convert_xml_float_2_int(self, v):
@@ -61,20 +59,22 @@ class ClassBalancer:
                             end_y=self.convert_xml_float_2_int(xml_box[3].text))
                     predictions.append(
                             edgeiq.ObjectDetectionPrediction(box, 0, label, 0))
-                    class_predictions[label].append(edgeiq.ObjectDetectionPrediction(box, 0, label, 0))
-                
+                    class_predictions[label].append(
+                        edgeiq.ObjectDetectionPrediction(box, 0, label, 0))
+
             all_predictions.extend(predictions)
             frame_predictions[fname] = predictions
             self.dict_fnames_class = class_fnames
         return frame_predictions, all_predictions, class_predictions
 
     def get_dataset_stats(self):
-        frame_predictions,all_predictions,class_predictions = self.parse_voc_annotations()
-        self.class_distribution = {i:len(class_predictions[i]) for i in class_predictions.keys()} 
+        frame_predictions, all_predictions, class_predictions = self.parse_voc_annotations()
+        self.class_distribution = {i: len(class_predictions[i]) for i in class_predictions.keys()} 
         return self.class_distribution
-    
-    def get_balancing_stats(self, balance_threshold = 0.00):
-        balancing_base = min(self.class_distribution.items(),key = lambda x:x[1])
+
+    def get_balancing_stats(self, balance_threshold=0.00):
+        balancing_base = min(
+            self.class_distribution.items(), key=lambda x: x[1])
         class_fnames_list = list(self.dict_fnames_class.items())
         # print("Class fnames list length" , len(class_fnames_list))
 
@@ -83,41 +83,28 @@ class ClassBalancer:
 
         allowed_thresh = 1+balance_threshold
 
-        for k,v in self.class_distribution.items():
-            # print(k,v)
+        for k, v in self.class_distribution.items():
             if(v > allowed_thresh*balancing_base[1]):
                 balancing_classes[k] = v
-                # balancing_labels.append(k)
             else:
                 balanced_classes[k] = v
-                # balanced_labels.append(k)
-
-        # print("balancing classes",balancing_classes)
-        # print("balancing labels",balancing_labels)
-        # print("balanced classes",balanced_classes)
-        # print("balanced labels", balanced_labels)
 
         to_move_list = []
         processed_list = []
         while len(balancing_classes) != 0:
             if len(class_fnames_list) != 0:
-                idx = random.randint(0,len(class_fnames_list)-1)
+                idx = random.randint(0, len(class_fnames_list)-1)
                 frame_data = class_fnames_list[idx]
                 class_fnames_list.pop(idx)
                 processed_list.append(frame_data[0])
-
-                # print("selected frame", frame_data)
-                # print("balanced_classes",balanced_classes)
-                # print("balancing_classes",balancing_classes)
-                # print("Processed",len(processed_list))
 
                 if(len((set(balanced_classes.keys()) & set(frame_data[1].keys()))) == 0):
                     # print("Can think forward to delete")
                     to_update = {}
 
                     deletable_frame = True
-                    for k,v in frame_data[1].items():
-                        if(balancing_classes[k] <= allowed_thresh*balancing_base[1]):
+                    for k, v in frame_data[1].items():
+                        if(balancing_classes[k] <= allowed_thresh * balancing_base[1]):
                             balanced_classes[k] = balancing_classes[k]
                             deletable_frame = False
                             break
@@ -126,7 +113,7 @@ class ClassBalancer:
 
                     if(deletable_frame):
                         to_move_list.append(frame_data[0])
-                        for k,v in to_update.items():
+                        for k, v in to_update.items():
                             balancing_classes[k] -= v
                 # else:
                     # print("CAn't delete this")
@@ -134,52 +121,52 @@ class ClassBalancer:
                 new_class_distribution = {}
                 new_class_distribution.update(balanced_classes)
                 new_class_distribution.update(balancing_classes)
-                print("BEST ACHIEVABLE BALANCE",new_class_distribution)
-                return new_class_distribution,to_move_list
+                print("BEST ACHIEVABLE BALANCE", new_class_distribution)
+                return new_class_distribution, to_move_list
 
 #If successfully achieved required balancing 
         new_class_distribution = {}
         new_class_distribution.update(balanced_classes)
         new_class_distribution.update(balancing_classes)
-        print("SUCCESFULLY ACHIEVED REQUIRED BALANCE",new_class_distribution)
-        return new_class_distribution,to_move_list
+        print("SUCCESFULLY ACHIEVED REQUIRED BALANCE", new_class_distribution)
+        return new_class_distribution, to_move_list
 
     def _create_required_dirs(self):
         if not os.path.isdir(self.path_holdout_dir):
             os.mkdir(self.path_holdout_dir)
-        
+
         if not os.path.isdir(self.path_holdout_annotations):
             os.mkdir(self.path_holdout_annotations)
-        
+
         if not os.path.isdir(self.path_holdout_images):
             os.mkdir(self.path_holdout_images)
 
     def partition_balanced_dataset(self, to_move_list, **kwargs):
         self.path_holdout_dir = kwargs.get('output_dir', self.path_holdout_dir)
-        self.path_holdout_annotations = os.path.join(self.path_holdout_dir ,'Annotations')
-        self.path_holdout_images = os.path.join(self.path_holdout_dir,'JPEGImages')
+        self.path_holdout_annotations = os.path.join(
+            self.path_holdout_dir, 'Annotations')
+        self.path_holdout_images = os.path.join(
+            self.path_holdout_dir, 'JPEGImages')
 
         self._create_required_dirs()
 
         for i in to_move_list:
-            y,_exten = i.split('.')
-            # print(y,_)
-            dest = shutil.move(self.path_annotations + '/' + y + '.' + _exten,self.path_holdout_annotations)
-            dest = shutil.move(self.path_images + '/' + y + '.' + 'jpg',self.path_holdout_images)
-
-        
+            y, _exten = i.split('.')
+            dest = shutil.move(
+                os.path.join(
+                    self.path_annotations, os.sep, y, '.' + _exten, self.path_holdout_annotations))
+            dest = shutil.move(
+                os.path.join(self.path_images, os.sep, y,'.', 'jpg',self.path_holdout_images))
 
 
 ########### TESTING ##################
 
-input_dir = '3_cam/all-side-cams-merged-dataset'
+input_dir = 'dataset_sample_584-2/'  # TODO: replace with your input directory
 balancer = ClassBalancer(input_dir)
 class_distri = balancer.get_dataset_stats()
-print("Initial Class Distribution",class_distri)
+print("Initial Class Distribution", class_distri)
 
 new_class_distri, move_list = balancer.get_balancing_stats()
 
-print("new dist",new_class_distri)
-print("To be moved list", len(move_list))
-
-# balancer.partition_balanced_dataset(move_list)
+print("new dist", new_class_distri)
+print("To be moved list", move_list)
--- synthetic-objects/synth.py
+++ synthetic-objects/synth.py
@@ -9,65 +9,56 @@ import xml.etree.ElementTree as ET
 from PIL import Image
 
 
+images = glob.glob(
+    os.path.join(
+        os.getcwd, 'Classes', os.sep, '**', os.sep, '*.png'), recursive=True)
+annotation_file = ET.parse("result.xml")
 
-images = glob.glob('./Classes/**/*.png', recursive=True)
-annotation_file = ET.parse("/Users/toddgleed/aalwaysAI_stuff/datasets/Unity/ManualSynth/result.xml")
-
-annotation_path = "Users/toddgleed/aalwaysAI_stuff/datasets/Unity/ManualSynth/Annotations/"
+annotation_path = os.path.join(os.getcwd, os.sep, "Annotations", os.sep)
 annotation_name = "result.xml"
-
-#annotation_file.write(os.path.join(annotation_path, annotation_name))
 
 numImages = 3250
 
 i = 1
-while i<numImages:
-    # reset the image background - future can randomize through different bg images
-    path = r"/Users/toddgleed/aalwaysAI_stuff/datasets/Unity/ManualSynth/backgrounds/"
-    random_filename = random.choice([
-        x for x in os.listdir(path)
-            if os.path.isfile(os.path.join(path, x))
-    ])
-    
+while i < numImages:
+    # reset the image background
+    path = r""
+    random_filename = random.choice(
+        [x for x in os.listdir(path)
+            if os.path.isfile(os.path.join(path, x))]
+    )
+
     img_bg = Image.open("./backgrounds/" + random_filename)
-    width, height = img_bg.size 
+    width, height = img_bg.size
 
-    
-
-    #Randomly determine number of objects to put on image
+    # Randomly determine number of objects to put on image
     a = random.randint(1,5)
 
-    #Randomly sample examples of all class examples
+    # Randomly sample examples of all class examples
     sample_list = random.choices(images, k=a)
     ai = 0
     bbs = []
     for img in sample_list:
         class_name = img.split('/')[2]
-        
+
         img = Image.open(img).convert("RGBA")
         x, y = img.size
         lane = int(round(width/6))
         xstart = 80 + (ai * lane)
         print('x:' + str(width) + 'xstart:' + str(xstart))
-        
+
         x1 = random.randint(xstart, xstart+lane)
         y1 = random.randint(80, height-180)
-        x3 = x+x1
-        y3 = y+y1
-        x2 = min(x+x1, width)
-        y2 = min(y+y1, height)
+        x3 = x + x1
+        y3 = y + y1
+        x2 = min(x + x1, width)
+        y2 = min(y + y1, height)
         img_bg.paste(img, (x1, y1, x3, y3), img)
 
         bbs.append(BoundingBox(x1=x1, y1=y1, x2=x2, y2=y2))
-        ai+=1
-        #root.append(element)
+        ai += 1
 
-        #subelement = root[0][1].makeelement('name')
-        #ET.SubElement(root[1],'name')
-        #root[1][0].text = class_name
-
-    #Create an XMLFile
-    #new_annotation_name = annotation_file
+    # Create an XMLFile
     new_annotation_file = cp.deepcopy(annotation_file)
     root = new_annotation_file.getroot()
     for bbx in bbs:
@@ -92,14 +83,13 @@ while i<numImages:
         ymin.text = str(bbx.y1)
         ymax.text = str(bbx.y2)
 
-
-    
     new_annotation_file.find('filename').text = "result" + str(i) + ".jpg"
     for w in root.iter('width'):
         w.text = str(width)
     for h in root.iter('height'):
         h.text = str(height)
-    new_annotation_file.write("Annotations/result" + str(i) + ".xml")  
-         
-    img_bg.save('JPEGImages/result' + str(i) + '.jpg')
+    new_annotation_file.write(
+        "Annotations", os.sep, "result" + str(i) + ".xml")
+
+    img_bg.save('JPEGImages", os.sep, "result' + str(i) + '.jpg')
     i += 1
